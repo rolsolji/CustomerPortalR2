ed21adf940ce4797b857ff55dbdab0143ddc6837
Rolando Solano <rsolano@gmail.com>
2020-07-29 21:31:48 -0600

    RS - Add form-add-shipment component

---
+++ ClientApp/src/app/pages/ui/forms/form-add-shipment/form-add-shipment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vex-form-add-shipment',
+  templateUrl: './form-add-shipment.component.html',
+  styleUrls: ['./form-add-shipment.component.scss']
+})
+export class FormAddShipmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ ClientApp/src/app/pages/ui/forms/form-add-shipment/form-add-shipment.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormAddShipmentRoutingModule } from './form-add-shipment-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormAddShipmentRoutingModule
+  ]
+})
+export class FormAddShipmentModule { }
